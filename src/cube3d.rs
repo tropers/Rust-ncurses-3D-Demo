087d96452ee8fdbe2a96fe1cc6103d5ce9460138
tropers <tristan.ropers@gmail.com>
2024-08-15 21:51:11 +0200

    Move functions for 3d calculations into respective implementations

--- src/cube3d.rs
+++ src/cube3d.rs
@@ -28,6 +28,95 @@ struct Vec3 {
     z: f32,
 }
 
+impl Vec3 {
+    fn project(&self) -> Vec3 {
+        let near: f32 = 0.1;
+        let far: f32 = 1000.0;
+        let fov: f32 = 90.0;
+        let aspect_ratio: f32;
+        unsafe {
+            aspect_ratio = crate::SCREEN_HEIGHT / crate::SCREEN_WIDTH;
+        }
+
+        let fov_rad: f32 = 1.0 / f32::tan(fov * 0.5 / 180.0 * std::f32::consts::PI);
+
+        let proj_matrix = Matrix4x4 {
+            data: [
+                [aspect_ratio * fov_rad, 0.0, 0.0, 0.0],
+                [0.0, fov_rad, 0.0, 0.0],
+                [0.0, 0.0, far / (far - near), 1.0],
+                [0.0, 0.0, (-far * near) / (far - near), 0.0],
+            ],
+        };
+
+        
+        proj_matrix.multiply_vec3(self)
+    }
+
+    fn rotate_x(&self, theta: f32) -> Vec3 {
+        let rotate_x_matrix = Matrix4x4 {
+            data: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, f32::cos(theta), f32::sin(theta), 0.0],
+                [0.0, -f32::sin(theta), f32::cos(theta), 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+        };
+
+        rotate_x_matrix.multiply_vec3(self)
+    }
+
+    fn rotate_y(&self, theta: f32) -> Vec3 {
+        let rotate_z_matrix = Matrix4x4 {
+            data: [
+                [f32::cos(theta), 0.0, f32::sin(theta), 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [-f32::sin(theta), 0.0, f32::cos(theta), 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+        };
+
+        rotate_z_matrix.multiply_vec3(self)
+    }
+
+    fn rotate_z(&self, theta: f32) -> Vec3 {
+        let rotate_z_matrix = Matrix4x4 {
+            data: [
+                [f32::cos(theta), f32::sin(theta), 0.0, 0.0],
+                [-f32::sin(theta), f32::cos(theta), 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+        };
+
+        rotate_z_matrix.multiply_vec3(self)
+    }
+
+    fn translate_x(&self, x: f32) -> Vec3 {
+        Vec3 {
+            x: self.x + x,
+            y: self.y,
+            z: self.z,
+        }
+    }
+
+    fn translate_y(&self, y: f32) -> Vec3 {
+        Vec3 {
+            x: self.x,
+            y: self.y + y,
+            z: self.z,
+        }
+    }
+
+    fn translate_z(&self, z: f32) -> Vec3 {
+        Vec3 {
+            x: self.x,
+            y: self.y,
+            z: self.z + z,
+        }
+    }
+}
+
 #[derive(Clone)]
 struct Vertex {
     position: Vec3,
@@ -49,13 +138,259 @@ struct Matrix4x4 {
     data: [[f32; 4]; 4],
 }
 
+impl Matrix4x4 {
+    fn multiply_vec3(&self, vec: &Vec3) -> Vec3 {
+        let mut return_vector = Vec3 {
+            x: vec.x * &self.data[0][0]
+                + vec.y * &self.data[1][0]
+                + vec.z * &self.data[2][0]
+                + &self.data[3][0],
+            y: vec.x * &self.data[0][1]
+                + vec.y * &self.data[1][1]
+                + vec.z * &self.data[2][1]
+                + &self.data[3][1],
+            z: vec.x * &self.data[0][2]
+                + vec.y * &self.data[1][2]
+                + vec.z * &self.data[2][2]
+                + &self.data[3][2],
+        };
+
+        let w = vec.x * &self.data[0][3]
+            + &self.data[1][3]
+            + vec.z * &self.data[2][3]
+            + &self.data[3][3];
+
+        if w != 0.0 {
+            return_vector.x /= w;
+            return_vector.y /= w;
+            return_vector.z /= w;
+        }
+
+        return_vector
+    }
+}
+
 #[derive(Clone)]
 struct Triangle {
     vertices: Vec<Vertex>,
 }
 
+impl Triangle {
+    fn project(&self) -> Triangle {
+        Triangle {
+            vertices: self
+                .vertices
+                .iter()
+                .map(|vertex| -> Vertex {
+                    Vertex {
+                        position: vertex.position.project(),
+                    }
+                })
+                .collect(),
+        }
+    }
+
+    fn rotate_x(&self, theta: f32) -> Triangle {
+        Triangle {
+            vertices: self
+                .vertices
+                .iter()
+                .map(|vertex| -> Vertex {
+                    Vertex {
+                        position: vertex.position.rotate_x(theta),
+                    }
+                })
+                .collect(),
+        }
+    }
+
+    fn rotate_y(&self, theta: f32) -> Triangle {
+        Triangle {
+            vertices: self
+                .vertices
+                .iter()
+                .map(|vertex| -> Vertex {
+                    Vertex {
+                        position: vertex.position.rotate_y(theta),
+                    }
+                })
+                .collect(),
+        }
+    }
+
+    fn rotate_z(&self, theta: f32) -> Triangle {
+        Triangle {
+            vertices: self
+                .vertices
+                .iter()
+                .map(|vertex| -> Vertex {
+                    Vertex {
+                        position: vertex.position.rotate_z(theta),
+                    }
+                })
+                .collect(),
+        }
+    }
+
+    fn translate_x(&self, x: f32) -> Triangle {
+        Triangle {
+            vertices: self
+                .vertices
+                .iter()
+                .map(|vertex| -> Vertex {
+                    Vertex {
+                        position: vertex.position.translate_x(x),
+                    }
+                })
+                .collect(),
+        }
+    }
+
+    fn translate_y(&self, y: f32) -> Triangle {
+        Triangle {
+            vertices: self
+                .vertices
+                .iter()
+                .map(|vertex| -> Vertex {
+                    Vertex {
+                        position: vertex.position.translate_y(y),
+                    }
+                })
+                .collect(),
+        }
+    }
+
+    fn translate_z(&self, z: f32) -> Triangle {
+        Triangle {
+            vertices: self
+                .vertices
+                .iter()
+                .map(|vertex| -> Vertex {
+                    Vertex {
+                        position: vertex.position.translate_z(z),
+                    }
+                })
+                .collect(),
+        }
+    }
+
+    fn draw(&self, window: &pancurses::Window) {
+        // Scale the vertexs x and y coordinates into the screen dimensions
+        let scaled_vertices: Vec<Vec2> = self
+            .vertices
+            .iter()
+            .map(|vertex: &Vertex| -> Vec2 { vertex.scale_into_screen_2d() })
+            .collect();
+
+        match scaled_vertices.len() {
+            TRIANGLE_VERTEX_COUNT => {
+                draw_line(
+                    window,
+                    &scaled_vertices[VERTEX_INDEX_1],
+                    &scaled_vertices[VERTEX_INDEX_2],
+                );
+                draw_line(
+                    window,
+                    &scaled_vertices[VERTEX_INDEX_2],
+                    &scaled_vertices[VERTEX_INDEX_3],
+                );
+                draw_line(
+                    window,
+                    &scaled_vertices[VERTEX_INDEX_1],
+                    &scaled_vertices[VERTEX_INDEX_3],
+                );
+            }
+            _ => {
+                eprintln!(
+                    "ERROR: Triangle is missing {} vertex / vertices!",
+                    TRIANGLE_VERTEX_COUNT - scaled_vertices.len()
+                );
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
 struct Mesh {
     triangles: Vec<Triangle>,
+}
+
+impl Mesh {
+    fn project(&self) -> Mesh {
+        Mesh {
+            triangles: self
+                .triangles
+                .iter()
+                .map(|triangle| -> Triangle { triangle.project() })
+                .collect(),
+        }
+    }
+    
+    fn rotate_x(&self, theta: f32) -> Mesh {
+        Mesh {
+            triangles: self
+                .triangles
+                .iter()
+                .map(|triangle| -> Triangle { triangle.rotate_x(theta) })
+                .collect(),
+        }
+    }
+    
+    fn rotate_y(&self, theta: f32) -> Mesh {
+        Mesh {
+            triangles: self
+                .triangles
+                .iter()
+                .map(|triangle| -> Triangle { triangle.rotate_y(theta) })
+                .collect(),
+        }
+    }
+    
+    fn rotate_z(&self, theta: f32) -> Mesh {
+        Mesh {
+            triangles: self
+                .triangles
+                .iter()
+                .map(|triangle| -> Triangle { triangle.rotate_z(theta) })
+                .collect(),
+        }
+    }
+    
+    fn translate_x(&self, x: f32) -> Mesh {
+        Mesh {
+            triangles: self
+                .triangles
+                .iter()
+                .map(|triangle| -> Triangle { triangle.translate_x(x) })
+                .collect(),
+        }
+    }
+    
+    fn translate_y(&self, y: f32) -> Mesh {
+        Mesh {
+            triangles: self
+                .triangles
+                .iter()
+                .map(|triangle| -> Triangle { triangle.translate_y(y) })
+                .collect(),
+        }
+    }
+    
+    fn translate_z(&self, z: f32) -> Mesh {
+        Mesh {
+            triangles: self
+                .triangles
+                .iter()
+                .map(|triangle| -> Triangle { triangle.translate_z(z) })
+                .collect(),
+        }
+    }
+    
+    fn draw(&self, window: &pancurses::Window) {
+        self.triangles.iter().for_each(|triangle| {
+            triangle.draw(window)
+        });
+    }
 }
 
 fn draw_line(window: &pancurses::Window, vec0: &Vec2, vec1: &Vec2) {
@@ -101,332 +436,6 @@ fn draw_line(window: &pancurses::Window, vec0: &Vec2, vec1: &Vec2) {
             vec0_rounded.y = vec0_rounded.y + sy;
         }
     }
-}
-
-fn multiply_matrix4x4_vec3(matrix: &Matrix4x4, vec: &Vec3) -> Vec3 {
-    let mut return_vector = Vec3 {
-        x: vec.x * matrix.data[0][0]
-            + vec.y * matrix.data[1][0]
-            + vec.z * matrix.data[2][0]
-            + matrix.data[3][0],
-        y: vec.x * matrix.data[0][1]
-            + vec.y * matrix.data[1][1]
-            + vec.z * matrix.data[2][1]
-            + matrix.data[3][1],
-        z: vec.x * matrix.data[0][2]
-            + vec.y * matrix.data[1][2]
-            + vec.z * matrix.data[2][2]
-            + matrix.data[3][2],
-    };
-
-    let w = vec.x * matrix.data[0][3]
-        + matrix.data[1][3]
-        + vec.z * matrix.data[2][3]
-        + matrix.data[3][3];
-
-    if w != 0.0 {
-        return_vector.x /= w;
-        return_vector.y /= w;
-        return_vector.z /= w;
-    }
-
-    return_vector
-}
-
-fn project_vec3(vec: &Vec3) -> Vec3 {
-    let near: f32 = 0.1;
-    let far: f32 = 1000.0;
-    let fov: f32 = 90.0;
-    let aspect_ratio: f32;
-    unsafe {
-        aspect_ratio = crate::SCREEN_HEIGHT / crate::SCREEN_WIDTH;
-    }
-
-    let fov_rad: f32 = 1.0 / f32::tan(fov * 0.5 / 180.0 * std::f32::consts::PI);
-
-    let proj_matrix = Matrix4x4 {
-        data: [
-            [aspect_ratio * fov_rad, 0.0, 0.0, 0.0],
-            [0.0, fov_rad, 0.0, 0.0],
-            [0.0, 0.0, far / (far - near), 1.0],
-            [0.0, 0.0, (-far * near) / (far - near), 0.0],
-        ],
-    };
-
-    multiply_matrix4x4_vec3(&proj_matrix, vec)
-}
-
-fn rotate_vec3_x(vec: &Vec3, theta: f32) -> Vec3 {
-    let rotate_x_matrix = Matrix4x4 {
-        data: [
-            [1.0, 0.0, 0.0, 0.0],
-            [0.0, f32::cos(theta), f32::sin(theta), 0.0],
-            [0.0, -f32::sin(theta), f32::cos(theta), 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ],
-    };
-
-    multiply_matrix4x4_vec3(&rotate_x_matrix, &vec)
-}
-
-fn rotate_vec3_y(vec: &Vec3, theta: f32) -> Vec3 {
-    let rotate_z_matrix = Matrix4x4 {
-        data: [
-            [f32::cos(theta), 0.0, f32::sin(theta), 0.0],
-            [0.0, 1.0, 0.0, 0.0],
-            [-f32::sin(theta), 0.0, f32::cos(theta), 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ],
-    };
-
-    multiply_matrix4x4_vec3(&rotate_z_matrix, &vec)
-}
-
-fn rotate_vec3_z(vec: &Vec3, theta: f32) -> Vec3 {
-    let rotate_z_matrix = Matrix4x4 {
-        data: [
-            [f32::cos(theta), f32::sin(theta), 0.0, 0.0],
-            [-f32::sin(theta), f32::cos(theta), 0.0, 0.0],
-            [0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ],
-    };
-
-    multiply_matrix4x4_vec3(&rotate_z_matrix, &vec)
-}
-
-fn translate_vec3_x(vec: &Vec3, x: f32) -> Vec3 {
-    Vec3 {
-        x: vec.x + x,
-        y: vec.y,
-        z: vec.z,
-    }
-}
-
-fn translate_vec3_y(vec: &Vec3, y: f32) -> Vec3 {
-    Vec3 {
-        x: vec.x,
-        y: vec.y + y,
-        z: vec.z,
-    }
-}
-
-fn translate_vec3_z(vec: &Vec3, z: f32) -> Vec3 {
-    Vec3 {
-        x: vec.x,
-        y: vec.y,
-        z: vec.z + z,
-    }
-}
-
-fn project_triangle(triangle: &Triangle) -> Triangle {
-    Triangle {
-        vertices: triangle
-            .vertices
-            .iter()
-            .map(|vertex| -> Vertex {
-                Vertex {
-                    position: project_vec3(&vertex.position),
-                }
-            })
-            .collect(),
-    }
-}
-
-fn rotate_triangle_x(triangle: &Triangle, theta: f32) -> Triangle {
-    Triangle {
-        vertices: triangle
-            .vertices
-            .iter()
-            .map(|vertex| -> Vertex {
-                Vertex {
-                    position: rotate_vec3_x(&vertex.position, theta),
-                }
-            })
-            .collect(),
-    }
-}
-
-fn rotate_triangle_y(triangle: &Triangle, theta: f32) -> Triangle {
-    Triangle {
-        vertices: triangle
-            .vertices
-            .iter()
-            .map(|vertex| -> Vertex {
-                Vertex {
-                    position: rotate_vec3_y(&vertex.position, theta),
-                }
-            })
-            .collect(),
-    }
-}
-
-fn rotate_triangle_z(triangle: &Triangle, theta: f32) -> Triangle {
-    Triangle {
-        vertices: triangle
-            .vertices
-            .iter()
-            .map(|vertex| -> Vertex {
-                Vertex {
-                    position: rotate_vec3_z(&vertex.position, theta),
-                }
-            })
-            .collect(),
-    }
-}
-
-fn translate_triangle_x(triangle: &Triangle, x: f32) -> Triangle {
-    Triangle {
-        vertices: triangle
-            .vertices
-            .iter()
-            .map(|vertex| -> Vertex {
-                Vertex {
-                    position: translate_vec3_x(&vertex.position, x),
-                }
-            })
-            .collect(),
-    }
-}
-
-fn translate_triangle_y(triangle: &Triangle, y: f32) -> Triangle {
-    Triangle {
-        vertices: triangle
-            .vertices
-            .iter()
-            .map(|vertex| -> Vertex {
-                Vertex {
-                    position: translate_vec3_y(&vertex.position, y),
-                }
-            })
-            .collect(),
-    }
-}
-
-fn translate_triangle_z(triangle: &Triangle, z: f32) -> Triangle {
-    Triangle {
-        vertices: triangle
-            .vertices
-            .iter()
-            .map(|vertex| -> Vertex {
-                Vertex {
-                    position: translate_vec3_z(&vertex.position, z),
-                }
-            })
-            .collect(),
-    }
-}
-
-fn draw_triangle(window: &pancurses::Window, triangle: &Triangle) {
-    // Scale the vertexs x and y coordinates into the screen dimensions
-    let scaled_vertices: Vec<Vec2> = triangle
-        .vertices
-        .iter()
-        .map(|vertex: &Vertex| -> Vec2 { vertex.scale_into_screen_2d() })
-        .collect();
-
-    match scaled_vertices.len() {
-        TRIANGLE_VERTEX_COUNT => {
-            draw_line(
-                window,
-                &scaled_vertices[VERTEX_INDEX_1],
-                &scaled_vertices[VERTEX_INDEX_2],
-            );
-            draw_line(
-                window,
-                &scaled_vertices[VERTEX_INDEX_2],
-                &scaled_vertices[VERTEX_INDEX_3],
-            );
-            draw_line(
-                window,
-                &scaled_vertices[VERTEX_INDEX_1],
-                &scaled_vertices[VERTEX_INDEX_3],
-            );
-        }
-        _ => {
-            eprintln!(
-                "ERROR: Triangle is missing {} vertex / vertices!",
-                TRIANGLE_VERTEX_COUNT - scaled_vertices.len()
-            );
-            std::process::exit(1);
-        }
-    }
-}
-
-fn project_mesh(mesh: &Mesh) -> Mesh {
-    Mesh {
-        triangles: mesh
-            .triangles
-            .iter()
-            .map(|triangle| -> Triangle { project_triangle(&triangle) })
-            .collect(),
-    }
-}
-
-fn rotate_mesh_x(mesh: &Mesh, theta: f32) -> Mesh {
-    Mesh {
-        triangles: mesh
-            .triangles
-            .iter()
-            .map(|triangle| -> Triangle { rotate_triangle_x(&triangle, theta) })
-            .collect(),
-    }
-}
-
-fn rotate_mesh_y(mesh: &Mesh, theta: f32) -> Mesh {
-    Mesh {
-        triangles: mesh
-            .triangles
-            .iter()
-            .map(|triangle| -> Triangle { rotate_triangle_y(&triangle, theta) })
-            .collect(),
-    }
-}
-
-fn rotate_mesh_z(mesh: &Mesh, theta: f32) -> Mesh {
-    Mesh {
-        triangles: mesh
-            .triangles
-            .iter()
-            .map(|triangle| -> Triangle { rotate_triangle_z(&triangle, theta) })
-            .collect(),
-    }
-}
-
-fn translate_mesh_x(mesh: &Mesh, x: f32) -> Mesh {
-    Mesh {
-        triangles: mesh
-            .triangles
-            .iter()
-            .map(|triangle| -> Triangle { translate_triangle_x(&triangle, x) })
-            .collect(),
-    }
-}
-
-fn translate_mesh_y(mesh: &Mesh, y: f32) -> Mesh {
-    Mesh {
-        triangles: mesh
-            .triangles
-            .iter()
-            .map(|triangle| -> Triangle { translate_triangle_y(&triangle, y) })
-            .collect(),
-    }
-}
-
-fn translate_mesh_z(mesh: &Mesh, z: f32) -> Mesh {
-    Mesh {
-        triangles: mesh
-            .triangles
-            .iter()
-            .map(|triangle| -> Triangle { translate_triangle_z(&triangle, z) })
-            .collect(),
-    }
-}
-
-fn draw_mesh(window: &pancurses::Window, mesh: &Mesh) {
-    mesh.triangles.iter().for_each(|triangle| {
-        draw_triangle(window, &triangle);
-    });
 }
 
 fn draw_text(window: &pancurses::Window, theta: f32) {
@@ -761,12 +770,13 @@ pub fn run_cube_demo(window: &pancurses::Window) {
     let mut clear = true;
 
     while t < 10.0 {
-        let rot_mesh_y = rotate_mesh_y(&cube, t);
-        let rot_mesh_z = rotate_mesh_z(&rot_mesh_y, t);
-        let trans_mesh_z = translate_mesh_z(&rot_mesh_z, 2.5);
-        let proj_mesh = project_mesh(&trans_mesh_z);
+        cube
+            .rotate_y(t)
+            .rotate_z(t)
+            .translate_z(2.5)
+            .project()
+            .draw(window);
 
-        draw_mesh(&window, &proj_mesh);
         draw_text(&window, t);
 
         // Move cursor out of the way
